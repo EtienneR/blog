b0fe212543faf2ee162719a41f407cfd3a03b089
Etienne Rouzeaud <etienne.rouzeaud@free.fr>
2019-06-08 15:53:26 +0200

    Création de la page  pour afficher un post

---
+++ src/templates/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <h1>{{ $page.post.title }}</h1>
+    <p><em>Posté le <span>{{ $page.post.date }}</span></em></p>
+    <article v-html="$page.post.content" />
+  </Layout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+  post: post (path: $path) {
+    title,
+    date (format: "D MMMM YYYY"),
+    content
+  }
+}
+</page-query>
