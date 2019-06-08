119f2fdecb97034851e4fa0c48a510310148344e
Etienne Rouzeaud <etienne.rouzeaud@free.fr>
2019-06-08 16:00:56 +0200

    Suppression du CSS de base

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="layout">
-    <header class="header">
+  <div class="container">
+    <header>
       <strong>
         <g-link to="/" title="Accueil">Etienne ROUZEAUD</g-link>
       </strong>
     </header>
-    <slot/>
+    <slot class="mt-4" />
   </div>
 </template>
 
@@ -16,31 +16,3 @@ query {
   }
 }
 </static-query>
-
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
